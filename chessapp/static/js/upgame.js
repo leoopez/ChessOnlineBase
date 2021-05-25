f5502d627890478c4cf9efed10e0f6d077eed54f
leoopez <leoocode@gmail.com>
2021-05-24 22:44:12 -0700

    24052021

--- chessapp/static/js/upgame.js
+++ chessapp/static/js/upgame.js
@@ -1,14 +1,25 @@
 'use strict'
 
 ;(function (){
-    const btnOWn = document.getElementById("own-window");
-    const btnLichess = document.getElementById("lichess-window");
-    const btnChess = document.getElementById("chess-window");
-    const btnChess24 = document.getElementById("chess24-window");
-    const winLiches
+    let innerHtml;
+    const btnLichess = document.getElementById("btn-lichess-com");
+    const btnChess = document.getElementById("btn-chess-com");
+    const btnChess24 = document.getElementById("btn-chess24-com");
+    const btnOwn = document.getElementById("btn-own");
+
     let parentDiv = document.querySelector(".token-modal");
 
     btnLichess.addEventListener('click', function (){
-        parentDiv. =
+        console.log(this);
+        parentDiv.innerHTML = `<div class="input-group mb-3">
+                                  <input type="text" class="form-control url_value" value="Xj7wHcHx" aria-label="Recipient's username" aria-describedby="button-addon2">
+                                  <a class="btn btn-outline-secondary nav_link" type="button" id="button-addon2" href="/">Button</a>
+                                </div>`
+
+        document.querySelector(".nav_link").addEventListener('click',   function() {
+            this.href= `${document.querySelector(".url_value").value}`;
+        });
     })
+
+
 })()
